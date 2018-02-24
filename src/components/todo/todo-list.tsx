ca859f289327ad1906752597f7b01992cc3b6845
hitesh97 <khatri.hitesh@gmail.com>
2018-02-24 19:31:33 +0000

    added support for fonts

--- src/components/todo/todo-list.tsx
+++ src/components/todo/todo-list.tsx
@@ -1,14 +1,28 @@
 import * as React from 'react';
 import {ITodo} from '../../features/todo/interfaces';
+import FaBirthday from 'react-icons/lib/fa/birthday-cake';
+import FaBook from 'react-icons/lib/fa/book';
 
 interface Props {
     todos: ITodo[];
 }
 
 export class TodoList extends React.Component<Props, {}> {
+    renderItems(items: ITodo[]): any {
+        return (items.map((todo) => {
+        return (
+            <i key={todo.id}>
+                <FaBirthday />
+                <FaBook />
+                {todo.text}
+            </i>
+        );
+        }));
+    }
     render() {
         return (
-            <ul className="listStyle">{this.props.todos.map((todo) => <i key={todo.id}>{todo.text}</i>)}</ul>
-          );
+            <ul className="listStyle">
+            {this.renderItems(this.props.todos)}
+            </ul>);
     }
 }
