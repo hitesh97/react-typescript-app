f4787c0a667d3f3a5194130b779756a75f1bde98
hitesh97 <khatri.hitesh@gmail.com>
2018-02-26 09:42:46 +0000

    started Material Design application

--- src/features/counters/index.ts
+++ src/features/counters/index.ts
@@ -1,8 +1,6 @@
 // public API
-import * as countersActions from './actions';
-import * as countersSelectors from './selectors';
-import { reducer, State } from './reducer';
+import * as todoActions from './actions';
+import { ToDoreducer, ToDoState } from './reducer';
 // export * from './epics';
-import {ADD, INCREMENT} from './action-constants';
 
-export { reducer, State, countersActions, countersSelectors, ADD, INCREMENT };
+export { ToDoreducer, ToDoState, todoActions};
