b005a497cb9c690490b99826a5a12ef54d1978fd
hitesh97 <khatri.hitesh@gmail.com>
2018-03-08 11:42:07 +0000

    added grid to the page

--- src/components/main-page.tsx
+++ src/components/main-page.tsx
@@ -4,6 +4,7 @@ import withRoot from '../../src/withRoot';
 import AppTopBar from '../components/app-bar/app-bar';
 import {TodoManager, TodoSample} from './todo/todo-manager';
 import AppDrawer from '../components/app-drawer/app-drawer';
+import Grid from 'material-ui/Grid';
 import { Route } from 'react-router-dom';
 
 interface MainPageProps {
@@ -14,20 +15,27 @@ export class MainPage extends React.Component<MainPageProps & WithStyles<'root'>
     return (
       <>
         <AppTopBar loginButtonText="Hitesh" />
-        <Route
-            exact={true}
-            path={'/todo'}
-            render={(props) => (
-              <TodoManager {...props} />
-          )}
-        />
-        <Route
-            exact={true}
-            path={'/todo/sample'}
-            render={(props) => (
-              <TodoSample {...props} />
-          )}
-        />
+        <br /><br />
+        <Grid container={true}>
+          <Grid item={true} xs={1} />
+          <Grid item={true} xs={10}>
+            <Route
+                  exact={true}
+                  path={'/todo'}
+                  render={(props) => (
+                    <TodoManager {...props} />
+                )}
+            />
+            <Route
+                  exact={true}
+                  path={'/todo/sample'}
+                  render={(props) => (
+                    <TodoSample {...props} />
+                )}
+            />
+          </Grid>
+          <Grid item={true} xs={1} />
+        </Grid >
         <AppDrawer open={false} />
       </>
     );
